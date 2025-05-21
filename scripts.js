5007978d66c37c9b1f9a522248bc12bbbe2d1b1e
administradorky <uepkintikuyachay@gmail.com>
2025-05-21 15:27:38 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-function enviarMensaje(https://wa.me/qr/AVGYBBFTC7EUM1 ) {
+function enviarMensaje(https://wa.me/qr/AVGYBBFTC7EUM1) {
   alert("Mensaje enviado correctamente. Nos pondremos en contacto contigo pronto.");
 }
 
